078bae51bd6612fbb1ba93bbbd56228725003d94
jeremiahtabb <jeremiah.r29q7.tabb@gmail.com>
2021-10-09 17:58:36 -0400

    Finish making new ui scheme to add to-do's through pre-loaded common to-do's

--- src/components/zzold_UserShowTodoAdd.js
+++ src/components/zzold_UserShowTodoAdd.js
@@ -1,12 +1,12 @@
+
+   
 import { useEffect, useState } from "react";
 
 function UserShowTodoAdd({ userId, onAddDisplayTodo }) {
-  const [typeId, setTypeId] = useState("");
+  const [typeIx, setTypeIx] = useState("");
   const [types, setTypes] = useState([]);
-  const [cTodoIx, setCTodoIx] = useState("");
-  const [common_todos, setCommonTodos] = useState([]);
+  // const [todos, setTodos] = useState([]); For later w/ previour todos
   const [todo_name, setTodoName] = useState("");
-  const [c_todo_name, setCTodoName] = useState("");
   const [errors, setErrors] = useState([]);
 
   useEffect(() => {
@@ -17,18 +17,20 @@ function UserShowTodoAdd({ userId, onAddDisplayTodo }) {
       });
   }, []);
 
-  useEffect(() => {    
-    fetch("/common_todos")
-      .then((r) => r.json())
-      .then((j) => {
-        setCommonTodos(j);
-      });
-  }, []);
+  // useEffect(() => {    For later, we'll get his previous todos
+  //   fetch("/users/1")
+  //     .then((r) => r.json())
+  //     .then((j) => {
+  //       console.log(j);
+  //       setTodos(j.todos);
+  //     });
+  // }, []);
 
   function handleSubmit(e) {
     e.preventDefault();
+    const ix = Number(typeIx);
     const formData = {
-      type_id: Number(typeId),
+      type_id: types[ix].id,
       user_id: userId,
       todo_name: todo_name
     };
@@ -41,7 +43,7 @@ function UserShowTodoAdd({ userId, onAddDisplayTodo }) {
     }).then((r) => {
       if (r.ok) {
         r.json().then((todo) => {
-          if (c_todo_name !== todo_name) setTypeId("");
+          setTypeIx("");
           setErrors([]);
           onAddDisplayTodo(todo);
         });
@@ -51,23 +53,9 @@ function UserShowTodoAdd({ userId, onAddDisplayTodo }) {
     });
   }
 
-  function handleChangeCommonTodo(e) {
-    const cTodo = common_todos.find( ({id}) => {
-      return id === Number(e.target.value);
-    });
-
-    setCTodoIx(e.target.value);
-    if (e.target.value !== "") {
-      setCTodoName(cTodo.todo_name);
-      setTypeId(cTodo.type_id);
-      setTodoName(cTodo.todo_name);
-    }
-  }
-
   return (
     <form onSubmit={handleSubmit}>
       <h2>Add New To-do</h2>
-
       <div>
         <label htmlFor="todo_id">Enter one:</label>
         <input
@@ -76,42 +64,37 @@ function UserShowTodoAdd({ userId, onAddDisplayTodo }) {
           value={todo_name}
           onChange={(e) => setTodoName(e.target.value)}
         />
+        {/* <select
+          id="todo_id"
+          value={todoIx}
+          onChange={(e) => setTodoIx(e.target.value)}
+        >
+          <option value="">Select todo...</option>
+          {todos.map((todo, ix) => (
+            <option key={todo.id} value={ix}>
+              {todo.todo_name}
+            </option>
+          ))}
+        </select> */}
         <select
-          id="type_id"
-          value={typeId}
-          onChange={(e) => setTypeId(e.target.value)}
+          id="todo_id"
+          value={typeIx}
+          onChange={(e) => setTypeIx(e.target.value)}
         >
           <option value="">Select type...</option>
-          {types.map((type) => (
-            <option key={type.id} value={type.id}>
+          {types.map((type, ix) => (
+            <option key={type.id} value={ix}>
               {type.type_name}
             </option>
           ))}
         </select>
-      </div>
 
-      <div>
-        <select
-          id="common_todo_id"
-          value={cTodoIx}
-          onChange={(e) => handleChangeCommonTodo(e)}
-          onClick={(e) => handleChangeCommonTodo(e)}
-        >
-          <option value="">Select a previous or common to-do ...</option>
-          {common_todos.map((cTodo) => (
-            <option key={cTodo.id} value={cTodo.id}>
-              {cTodo.to_display}
-            </option>
-          ))}
-        </select>
       </div>
-
       {errors.map((err) => (
         <p key={err} style={{ color: "red" }}>
           {err}
         </p>
       ))}
-
       <button todo="submit">Submit</button>
     </form>
   );
